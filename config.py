7b8178eb45e874f37385b13ae997e608795d5579
kevmo <k.james.moore@gmail.com>
2014-12-13 10:14:15 -0600

    WIP: Midway through setting up Postgres db

--- config.py
+++ config.py
@@ -3,5 +3,31 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 #
-# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+# SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 # SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+SQLALCHEMY_DATABASE_URI = "postgresql://localhost/jpgur_dev"
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
