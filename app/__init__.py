4366ed22c6acfe5b1d869b87860cd012e736f903
kevmo <k.james.moore@gmail.com>
2014-12-13 13:33:15 -0600

    Configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
+import os
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config.from_object('config')
+
+print "CHECK ENVIRON APP SETTINGS: ", os.environ['APP_SETTINGS']
+
+app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
 from app import views
 from app import models
+
+# check environment config
+
+print os.environ['DATABASE_URL']
--- config.py
+++ config.py
@@ -6,15 +6,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 # SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
-# SQLALCHEMY_DATABASE_URI = "postgresql://localhost/jpgur_dev"
 
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
